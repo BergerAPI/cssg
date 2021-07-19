7f728dd45a7f3211de5f07662477374dc5e3b054
niclas <bergerbtw@gmail.com>
2021-07-19 21:36:58 +0200

    listing every post

---
+++ src/routes/posts/index.svelte
@@ -0,0 +1,8 @@
+<script>
+  import fs from "fs";
+  let posts = fs.readdirSync("static/posts");
+</script>
+
+{#each posts as post}
+  <a href={"posts/" + post.split(".")[0]}>{post.split(".")[0]}</a>
+{/each}
