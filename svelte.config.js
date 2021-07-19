cda7abdff0e3a1d63366032ec785f675b9230bdd
niclas <bergerbtw@gmail.com>
2021-07-19 20:05:22 +0200

    fixed deploy error?

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,11 @@
+import AdapterStatic from '@sveltejs/adapter-static'
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
+		adapter: new AdapterStatic()
 	}
 };
 
